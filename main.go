b22f17f8cba34d47bed125ec48337eb3a53ea11f
Altryd <michael_bor@mail.ru>
2024-10-08 14:26:56 +0400

    return err instead of panic everywhere, also done code with parsingConfig.verbose

--- main.go
+++ main.go
@@ -9,9 +9,18 @@ func main() {
 
 	print("Hellow\n")
 	client := osuParseMpLinks.NewHttpClient()
-	data := client.GetUserDataByUsernameOrId("9109550")
-	fmt.Println(data)
-	mplinkData, userData := client.ParseMplink("https://osu.ppy.sh/community/matches/111534249/", osuParseMpLinks.ParsingConfig{})
-	fmt.Println(mplinkData)
-	fmt.Println(userData)
+	data, err := client.GetUserDataByUsernameOrId("9109550")
+	if err != nil {
+		print(err)
+	} else {
+		fmt.Println("data from GetUserDataByUsernameOrId", data)
+	}
+	mplinkData, userData, err := client.ParseMplink("https://osu.ppy.sh/community/matches/111534249/", osuParseMpLinks.ParsingConfig{})
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	} else {
+		fmt.Println("data from ParseMpLink")
+		fmt.Println(mplinkData)
+		fmt.Println(userData)
+	}
 }
--- osu_api_usage/osu_api_usage.go
+++ osu_api_usage/osu_api_usage.go
@@ -30,18 +30,18 @@ func NewHttpClient() HttpClient {
 	return client
 }
 
-func (client *HttpClient) UpdateToken() {
+func (client *HttpClient) UpdateToken() error {
 	secretData := NewSecretData()
 	jsonData, err := json.Marshal(secretData)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	url := "https://osu.ppy.sh/oauth/token"
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
 	if err != nil {
-		panic(err)
+		return err
 	}
 	q := req.URL.Query()
 	req.Header.Add("Accept", "application/json")
@@ -50,21 +50,22 @@ func (client *HttpClient) UpdateToken() {
 
 	resp, err := client.Client.Do(req)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	var data map[string]interface{}
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	client.AccessToken = data["token_type"].(string) + " " + data["access_token"].(string)
+	return nil
 }
 
 func (client *HttpClient) reqUserData(usernameOrId string) (*http.Response, error) {
@@ -72,7 +73,7 @@ func (client *HttpClient) reqUserData(usernameOrId string) (*http.Response, erro
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	q := req.URL.Query()
 	req.Header.Add("Accept", "application/json")
@@ -83,31 +84,31 @@ func (client *HttpClient) reqUserData(usernameOrId string) (*http.Response, erro
 	return client.Client.Do(req)
 }
 
-func (client *HttpClient) GetUserDataByUsernameOrId(usernameOrId string) map[string]interface{} {
+func (client *HttpClient) GetUserDataByUsernameOrId(usernameOrId string) (map[string]interface{}, error) {
 	// получить данные юзера
 	resp, err := client.reqUserData(usernameOrId)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 	if resp.Status == "401" {
 		client.UpdateToken()
 		resp, err := client.reqUserData(usernameOrId)
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
 		defer resp.Body.Close()
 	}
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	var data map[string]interface{}
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return data
+	return data, nil
 }
 
 type ParsingConfig struct {
@@ -121,7 +122,7 @@ type ParsingConfig struct {
 func (client *HttpClient) reqMatchData(method string, url string, body io.Reader) (map[string]interface{}, error) {
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	q := req.URL.Query()
 	req.Header.Add("Accept", "application/json")
@@ -130,7 +131,7 @@ func (client *HttpClient) reqMatchData(method string, url string, body io.Reader
 	req.URL.RawQuery = q.Encode()
 	resp, err := client.Client.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 	respBody, err := io.ReadAll(resp.Body)
@@ -142,7 +143,7 @@ func (client *HttpClient) reqMatchData(method string, url string, body io.Reader
 	return data, err
 }
 
-func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConfig) ([]map[string]interface{}, map[int]interface{}) {
+func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConfig) ([]map[string]interface{}, map[int]interface{}, error) {
 	var matchUrl string
 	var matchId int
 	if parsingConfig.debug && len(matchArg) == 0 {
@@ -150,7 +151,7 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 		reader := bufio.NewReader(os.Stdin)
 		matchUrl, err := reader.ReadString('\n')
 		if err != nil {
-			panic(err)
+			return nil, nil, err
 		}
 		fmt.Println(matchUrl)
 	} else {
@@ -164,30 +165,30 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 		matchesRegexp := regexp.MustCompile(`matches/\d+`)
 		allSubstr := matchesRegexp.FindAllString(matchArg, -1)
 		if allSubstr == nil {
-			panic(errors.New("invalid link: cannot find matches/"))
+			 return nil, nil, errors.New("invalid link: cannot find matches/")
 		}
 		endOfUrl := allSubstr[0]
 		splitUrl := strings.Split(endOfUrl, "/")
 		if len(splitUrl) != 2 {
-			panic(errors.New("invalid link: can't find match id"))
+			 return nil, nil, errors.New("invalid link: can't find match id")
 		}
 		var matchIdStr = splitUrl[1]
 		var err error
 		matchId, err = strconv.Atoi(matchIdStr)
 		if err != nil {
-			panic(errors.New("invalid link: can't convert matchIdStr to int"))
+			 return nil, nil, errors.New("invalid link: can't convert matchIdStr to int")
 		}
 	} else {
 		var err error
 		matchId, err = strconv.Atoi(matchArg)
 		if err != nil {
-			panic(errors.New("invalid link: can't convert matchArg to int"))
+			return nil, nil, errors.New("invalid link: can't convert matchArg to int")
 		}
 	}
 	url := fmt.Sprintf("https://osu.ppy.sh/api/v2/matches/%d", matchId)
 	data, err := client.reqMatchData(http.MethodGet, url, nil)
 	if err != nil {
-		panic(fmt.Sprintf("can't request match data: %s", err))
+		return nil, nil, errors.New(fmt.Sprintf("can't request match data: %s", err))
 	}
 	// START WORKING WITH DATA
 	var userDict map[int]interface{} // for user data
@@ -196,11 +197,11 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 
 	firstEventId, ok := data["first_event_id"].(float64)
 	if ok != true {
-		panic(errors.New("can't convert first_event_id to float64"))
+		return nil, nil, errors.New("can't convert first_event_id to float64")
 	}
 	lastEventId, ok := data["latest_event_id"].(float64)
 	if ok != true {
-		panic(errors.New("can't convert latest_event_id to float64"))
+		return nil, nil, errors.New("can't convert latest_event_id to float64")
 	}
 	eventId := firstEventId
 	for eventId < lastEventId {
@@ -211,22 +212,22 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 		url := fmt.Sprintf("https://osu.ppy.sh/api/v2/matches/%d", matchId)
 		matchData, err := client.reqMatchData(http.MethodGet, url, bytes.NewBuffer(jsonDataForQuery))
 		if err != nil {
-			panic(fmt.Sprintf("can't request match data by url: %s; error: %s", url, err))
+			return nil, nil, errors.New(fmt.Sprintf("can't request match data by url: %s; error: %s", url, err))
 		}
 		dataUsers, ok := matchData["users"].([]interface{})
 		if ok != true {
-			panic(errors.New("can't convert data['users'] to list of maps"))
+			return nil, nil, errors.New("can't convert data['users'] to list of maps")
 		}
 		for _, user := range dataUsers {
 			dataUserDict, ok := user.(map[string]interface{})
 			if ok != true {
-				panic(errors.New("can't convert userDict to map"))
+				 return nil, nil, errors.New("can't convert userDict to map")
 			}
 
 			userIdInterface := dataUserDict["id"]
 			userIdFloat, ok := userIdInterface.(float64)
 			if ok != true {
-				panic(errors.New("can't convert userId to float"))
+				 return nil, nil, errors.New("can't convert userId to float")
 			}
 			userId := int(userIdFloat)
 			_, ok = userDict[userId]
@@ -241,9 +242,8 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 		for _, event := range eventsData {
 			dataEventDict, ok := event.(map[string]interface{})
 			if ok != true {
-				panic(errors.New("can't convert event to map"))
+				return nil, nil, errors.New("can't convert event to map")
 			}
-			// fmt.Println(dataEventDict["detail"])
 			dataEventDetailDict, ok := dataEventDict["detail"].(map[string]interface{})
 			if ok != true {
 				continue
@@ -266,14 +266,14 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 		}
 		lastEventInBatchDict, ok := eventsData[len(eventsData)-1].(map[string]interface{})
 		if ok != true {
-			panic(errors.New("can't convert lastEventInBatch to map[string]interface{}"))
+			return nil, nil, errors.New("can't convert lastEventInBatch to map[string]interface{}")
 		}
 		eventId, ok = lastEventInBatchDict["id"].(float64)
 		if ok != true {
-			panic(errors.New("can't convert lastEventInBatch['id'] to float64"))
+			return nil, nil, errors.New("can't convert lastEventInBatch['id'] to float64")
 		}
 	}
-	fmt.Println("Scores count:", len(allScoresList))
+
 	if parsingConfig.warmups > 0 {
 		allScoresList = allScoresList[parsingConfig.warmups:]
 	}
@@ -283,21 +283,21 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 	for _, scoreStruct := range allScoresList {
 		scoreStructDict := scoreStruct
 		if ok != true {
-			panic(errors.New("can't convert scoreStruct to map"))
+			return nil, nil, errors.New("can't convert scoreStruct to map")
 		}
 		beatmapIdFloat, ok := scoreStructDict["beatmap_id"].(float64)
 		if ok != true {
-			panic(errors.New("can't convert beatmapId to float64"))
+			return nil, nil, errors.New("can't convert beatmapId to float64")
 		}
 		beatmapId := int(beatmapIdFloat)
 		scores, ok := scoreStructDict["scores"].([]interface{})
 		if ok != true {
-			panic(errors.New("can't convert scores to list of maps"))
+			return nil, nil, errors.New("can't convert scores to list of maps")
 		}
 		for _, score := range scores {
 			scoreDict, ok := score.(map[string]interface{})
 			if ok != true {
-				panic(errors.New("can't convert score to map"))
+				return nil, nil, errors.New("can't convert score to map")
 			}
 			userIdFloat := scoreDict["user_id"].(float64)
 			userId := int(userIdFloat)
@@ -307,7 +307,7 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 			if hasPlayedTwice {
 				oldBeatmapScoreDict, ok := playedMaps[beatmapId].(map[string]interface{})
 				if ok != true {
-					panic(errors.New("can't convert playedMap[beatmapId] to map"))
+					return nil, nil, errors.New("can't convert playedMap[beatmapId] to map")
 				}
 				oldScore := oldBeatmapScoreDict["score"].(float64)
 				if oldScore < scoreDict["score"].(float64) {
@@ -316,21 +316,20 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 			} else {
 				playedMaps[beatmapId] = scoreDict["score"].(float64)
 			}
-			// fmt.Println(playedMaps)
 		}
 	}
 	if parsingConfig.matchcostStandard == 0 {
 		parsingConfig.matchcostStandard = 500000
 	}
-	for userId, userDetails := range userDict {
+	for _, userDetails := range userDict {
 		mapsPlayed := 0
 		userDetailsDict, ok := userDetails.(map[string]interface{})
 		if ok != true {
-			panic(errors.New("can't convert userDict to map"))
+			return nil, nil, errors.New("can't convert userDict to map")
 		}
 		userPlayedMaps, ok := userDetailsDict["played_maps"].(map[int]interface{})
 		if ok != true {
-			panic(errors.New("can't convert userDict to map"))
+			return nil, nil, errors.New("can't convert userDict to map")
 		}
 		userDetailsDict["score_sum"] = 0
 		userDictScoreSum := float64(userDetailsDict["score_sum"].(int))
@@ -338,7 +337,7 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 			mapsPlayed += 1
 			score, ok := scoreStruct.(float64)
 			if ok != true {
-				panic(errors.New("can't convert scoreStruct to float64"))
+				return nil, nil, errors.New("can't convert scoreStruct to float64")
 			}
 			userDictScoreSum += score
 		}
@@ -347,34 +346,42 @@ func (client *HttpClient) ParseMplink(matchArg string, parsingConfig ParsingConf
 		if len(userPlayedMaps) > 0 {
 			userDetailsDict["average_score"] = userDictScoreSum / float64(mapsPlayed)
 		}
-		fmt.Println(mapsPlayed, userId, userPlayedMaps)
+		// fmt.Println(mapsPlayed, userId, userPlayedMaps)
 	}
-	var averageScoresList [][]float64
-	fmt.Println(averageScoresList)
-	//sort.Sort(userDict)
-	for userId, userDetails := range userDict {
-		userDetailsDict := userDetails.(map[string]interface{})
-		fmt.Println(userDetailsDict)
-		userDetailsAvgScore := userDetailsDict["average_score"].(float64)
-		newEntry := []float64{float64(userId), userDetailsAvgScore}
-		averageScoresList= append(averageScoresList, newEntry)
-		fmt.Println(averageScoresList[0])
-		//averageScoresList.append(userDetailsDict["average_score"].(float64))
-	}
-	sort.Slice(averageScoresList, func(i, j int) bool {
-		return averageScoresList[i][1] > averageScoresList[j][1]  // DESC
-	})
-	if !parsingConfig.verbose {
+	if parsingConfig.verbose {
+		fmt.Println("All scores count:", len(allScoresList))
+		var averageScoresList [][]float64
+		// fmt.Println(averageScoresList)
+		//sort.Sort(userDict)
+		for userId, userDetails := range userDict {
+			userDetailsDict := userDetails.(map[string]interface{})
+			// fmt.Println(userDetailsDict)
+			userDetailsAvgScore := userDetailsDict["average_score"].(float64)
+			newEntry := []float64{float64(userId), userDetailsAvgScore}
+			averageScoresList= append(averageScoresList, newEntry)
+			// fmt.Println(averageScoresList[0])
+			//averageScoresList.append(userDetailsDict["average_score"].(float64))
+		}
+		sort.Slice(averageScoresList, func(i, j int) bool {
+			return averageScoresList[i][1] > averageScoresList[j][1]  // DESC
+		})
 		for _, sortedEntry := range averageScoresList {
 			userId := int(sortedEntry[0])
 			userDetailsDict := userDict[userId].(map[string]interface{})
 			avgScore := userDetailsDict["average_score"].(float64)
 			username := userDetailsDict["username"].(string)
-			fmt.Println(username, avgScore)
+			playedMaps := userDetailsDict["played_maps"].(map[int]interface{})
+			scoreSum := int(userDetailsDict["score_sum"].(float64))
+			fmt.Printf("username: %s (id: %d); avg. score: %f ; match cost: %f ; played maps: %d ; score sum: %d \n",
+				username, userId, avgScore, avgScore / float64(parsingConfig.matchcostStandard), len(playedMaps), scoreSum)
+			//print(f"avg.score: {user_details['average_score']}, "
+			//f"match_cost: {user_details['average_score'] / matchcost_etalon}, "
+			//f"played maps: {len(user_details['played_maps'])} "
+			//f"score sum: {user_details['score_sum']}  by {user_details['username']}, user id: {user_id}")
 			// avgScore := userDict[userId]
 		}
 	}
 	// fmt.Println("user dict:", userDict)
 	// TODO: refactor
-	return allScoresList, userDict
+	return allScoresList, userDict, nil
 }
